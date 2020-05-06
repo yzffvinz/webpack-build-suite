a15b9c7419de48541e695d65bcb4c61e4b8359c5
liuwenzhe <liuwenzhe1@qq.com>
2020-05-06 22:43:16 +0800

    修改单元测试

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, `D:/DevWorkspace/webpack-mine/Charpert4/builder-webpack/test/smoke/template/src/index/index.js`);
-    assert.equal(baseConfig.entry.search, `D:/DevWorkspace/webpack-mine/Charpert4/builder-webpack/test/smoke/template/src/search/index.js`);
+    assert.equal(baseConfig.entry.index.indexOf(`D:/DevWorkspace/webpack-mine/Charpert4/builder-webpack/test/smoke/template/src/index/index.js`) !== -1, true);
+    assert.equal(baseConfig.entry.search.indexOf(`D:/DevWorkspace/webpack-mine/Charpert4/builder-webpack/test/smoke/template/src/search/index.js`) !== -1, true);
   })
 });
