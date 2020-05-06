4d19ff0831a258a53bd3c7432c653474025a2f8c
liuwenzhe <liuwenzhe1@qq.com>
2020-05-07 00:14:05 +0800

    修改测试用例

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,13 +1,10 @@
-
 const assert = require('assert');
-
-process.chdir(path.join(__dirname, 'smoke/template'));
 
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index.indexOf(`builder-webpack/test/smoke/template/src/index/index.js`) !== -1, true);
-    assert.equal(baseConfig.entry.search.indexOf(`builder-webpack/test/smoke/template/src/search/index.js`) !== -1, true);
+    assert.equal(baseConfig.entry.index.indexOf(`webpack-build-suite/test/smoke/template/src/index/index.js`) !== -1, true);
+    assert.equal(baseConfig.entry.search.indexOf(`webpack-build-suite/test/smoke/template/src/search/index.js`) !== -1, true);
   })
 });
